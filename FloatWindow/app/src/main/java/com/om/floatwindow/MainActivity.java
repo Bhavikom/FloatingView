6f5ec146d23e3df99915dc160f2f55893eaff878
Bhavikom <bhavikmakvanarock@gmail.com>
2020-03-03 11:21:33 +0530

    => commited on 3rd march

---
+++ FloatWindow/app/src/main/java/com/om/floatwindow/MainActivity.java
@@ -0,0 +1,48 @@
+package com.om.floatwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 2084;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            askPermission();
+        }
+
+        findViewById(R.id.buttonCreateWidget).setOnClickListener(this);
+    }
+
+
+    private void askPermission() {
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, SYSTEM_ALERT_WINDOW_PERMISSION);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            startService(new Intent(MainActivity.this, FloatingViewService.class));
+            finish();
+        } else if (Settings.canDrawOverlays(this)) {
+            startService(new Intent(MainActivity.this, FloatingViewService.class));
+            finish();
+        } else {
+            askPermission();
+            Toast.makeText(this, "You need System Alert Window Permission to do this", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
